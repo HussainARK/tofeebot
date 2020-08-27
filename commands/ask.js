5274a971a9b8714cebc7196f5796f0f4146a72f8
HussainARK <hussain.ark@yahoo.com>
2020-08-27 13:28:43 +0300

    Fixed another bug

--- commands/ask.js
+++ commands/ask.js
@@ -2,14 +2,19 @@ module.exports = {
     name: "ask",
     description: 'ask for something',
     execute(message, args) {
-        message.channel.send("I don't know, Ask your Mama" || 
-                            "Yes, That's a Fact" || 
-                            "Probably No??" || 
-                            "No!" || 
-                            "YES!" ||
-                            `Don't ask Me that Question!`,
-                            'Sadly, No',
-                            'Sadly, Yes',
-                            "I don't have Enough Time to answer This");
+        const answers = [
+            "I don't know, Ask your Mama",
+            "Yes, That's a Fact",
+            "Probably No??",
+            "No!",
+            "YES!",
+            `Don't ask Me that Question!`,
+            'Sadly, No',
+            'Sadly, Yes',
+            "I don't have Enough Time to answer This",
+            "Sorry but this is a Hard Question"
+        ]
+
+        message.channel.send(answers[Math.floor(Math.random()*items.length)]);
     }
 }
