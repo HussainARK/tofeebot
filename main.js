5f36cdb8ad453ab811784c8bc3e6978d6255d79a
HussainARK <hussain.ark@yahoo.com>
2020-08-27 14:57:17 +0300

    Finishing a Problem

--- main.js
+++ main.js
@@ -22,13 +22,13 @@ client.once('ready', () => {
         status: "online",
         activity: {
             name: "t/help",
-			type: "WATCHING"
+            type: "WATCHING"
         }
     });
 });
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix.toLowerCase()) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
