20f730b51f0f93d2f97dbb5de2708d6040cbbcba
HussainARK <hussain.ark@yahoo.com>
2020-09-16 11:14:58 +0300

    lmao nothing

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,5 @@ module.exports = {
     execute(message, args) {
         if (args == '') return message.channel.send('Bruh do you really want me to say Something? ENTER SOME TEXT AFTER THE COMMAND');
         message.channel.send(args.join(' '));
-        message.delete(1000).catch(console.error);
     }
 }
